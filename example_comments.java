b246787aac9aa0aaeba066fc8cd2bcf5c202cf1f
SanketSJ7 <sj7.sanketjadhav@gmail.com>
2024-06-24 19:31:11 +0530

    Add files via upload

---
+++ example_comments.java
@@ -0,0 +1,24 @@
+package Tasks;
+
+public class example_comments 
+
+{
+     public static void main(String[] args) 
+     	{
+			/*
+	        This is a multi-line comment.
+	        This is a multi-line comment.
+	        This is a multi-line comment.
+	        */  
+				
+	       System.out.println("Hi!!!");
+	       /**
+	        * Adds two numbers together.
+	         @html tags we used in comments.
+	         @H2 tag is used to highlight text.
+	        */         
+	       
+	       System.out.println("Today We learn Java comments!!!");
+		}
+
+}
