cbd72c0898c3d9303eac01817aa5ecf0f6169bf5
SanketSJ7 <sj7.sanketjadhav@gmail.com>
2024-06-24 17:29:49 +0530

    Second Commit

---
+++ example_getname.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class example_getname 	//"HERE THE CLASS IS DEFINED"
+{
+	public static void getName(String name) 	//"HERE 'getName' METHOD IS DECLARED AND DEFINED"
+		// "'(String name)' is the parameter taken in method 'getName' and return type is 'void'"
+		{
+			System.out.println(name); 	//"SYNTAX FOR PRINTING ANYTHING ON CONSOLE"			
+		}
+	
+	public static void main(String[] args) 		//"MAIN METHOD  IS DECLARED AND DEFINED"
+		{	
+		example_getname.getName("Sanket Jadhav"); 		//"METHOD CALL AND DIRECT GIVING OF STRING"
+		}
+
+}
